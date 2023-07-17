397a8ae45fb51cb0f7877a033eec9017dcf34880
pryority <matthewapryor@gmail.com>
2023-07-16 22:20:34 -0400

    fix: Tailwind styles build when air reruns

--- cmd/web/tailwind.config.js
+++ cmd/web/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  // purge: ["./views/**/*.html"],
-  content: ["./views/**/*.{html,js}"],
+  content: ["./views/**/*.html"],
   theme: {
     extend: {},
   },
