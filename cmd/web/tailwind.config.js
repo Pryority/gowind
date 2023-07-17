b79c796ac7178ef1754ba9bc5e7c2df605f02a1f
pryority <matthewapryor@gmail.com>
2023-07-16 20:17:42 -0400

    fix: tailwind build watch on live reload

--- cmd/web/tailwind.config.js
+++ cmd/web/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
+  // purge: ["./views/**/*.html"],
   content: ["./views/**/*.{html,js}"],
   theme: {
     extend: {},
